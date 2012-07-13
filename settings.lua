a80d142206359b41c9c3e2e6510333ea641021ff
Saxon Douglass <saxon@saxondouglass.com>
2012-07-13 12:56:59 +0930

    Added user settings file (screen size and fullscreen): settings.lua

---
+++ settings.lua
@@ -0,0 +1,16 @@
+-- USER SETTINGS
+-- Settings with -- in front are disabled.
+-- Remove the -- in front of a setting to enable it.
+
+return {
+
+-- SCREEN SIZE
+-- If enabled, sets the screen size to the specified dimensions.
+-- If disabled, the screen size will be automatically detected.
+  screen_width = 1920, screen_height = 1080,
+
+-- FULLSCREEN
+-- If disabled, defaults to true.
+  fullscreen = true,
+
+}
