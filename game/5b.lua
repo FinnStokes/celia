369539e6838efc7e609bca3c329121588009676a
Jaime von Schwarzburg <tmthesaurus@gmail.com>
2012-07-27 02:14:46 +0930

    new tea party

---
+++ game/5b.lua
@@ -0,0 +1,638 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  orientation = "orthogonal",
+  width = 23,
+  height = 30,
+  tilewidth = 128,
+  tileheight = 128,
+  properties = {
+    ["bgm"] = "ost/teaparty.ogg",
+    ["parallax1"] = "Background_TeaParty_Layer1.png",
+    ["parallax2"] = "Background_TeaParty_Layer2.png",
+    ["parallax3"] = "Background_TeaParty_Layer3.png"
+  },
+  tilesets = {
+    {
+      name = "TeaParty2",
+      firstgid = 1,
+      tilewidth = 128,
+      tileheight = 128,
+      spacing = 0,
+      margin = 0,
+      image = "TeaParty2.png",
+      imagewidth = 2048,
+      imageheight = 6144,
+      properties = {},
+      tiles = {
+        {
+          id = 16,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 17,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 18,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 32,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 33,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 34,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 64,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 65,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 66,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 67,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 80,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 81,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 82,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 145,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 147,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 161,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 163,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 194,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 195,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 210,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 211,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 225,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 226,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 227,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 240,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 241,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 242,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 243,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 273,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 275,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 277,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 279,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 305,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 336,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 337,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 338,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 368,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 369,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 384,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 385,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 417,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 418,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 433,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 434,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 465,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 466,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 481,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 482,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 497,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 498,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 513,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 514,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 545,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 546,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 547,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 561,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 562,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 563,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 595,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 600,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 601,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 608,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 611,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 613,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 614,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 615,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 617,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 624,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 627,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 631,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 633,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 657,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 658,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 659,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 660,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 689,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 690,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 691,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 692,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 721,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 722,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 723,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 724,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 754,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 755,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 756,
+          properties = {
+            ["solid"] = "true"
+          }
+        }
+      }
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      name = "Tile Layer 1",
+      x = 0,
+      y = 0,
+      width = 23,
+      height = 30,
+      visible = true,
+      opacity = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        258, 0, 0, 0, 0, 258, 258, 258, 258, 260, 260, 260, 260, 260, 0, 0, 0, 0, 0, 0, 0, 0, 260,
+        274, 0, 0, 0, 0, 274, 274, 274, 274, 276, 276, 276, 276, 276, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 0, 0, 0, 0, 258, 258, 258, 258, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 260, 260, 276,
+        274, 0, 0, 0, 0, 274, 274, 274, 274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276, 276, 276,
+        274, 258, 258, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 193, 0, 0, 0, 0, 0, 276,
+        274, 274, 274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 258, 258, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276, 276, 0, 0, 0, 0, 276,
+        274, 274, 274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 260, 260, 276,
+        274, 0, 0, 0, 0, 258, 258, 258, 258, 260, 260, 260, 260, 0, 0, 0, 0, 0, 0, 0, 276, 276, 276,
+        274, 0, 0, 0, 0, 274, 274, 274, 274, 276, 276, 276, 276, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 177, 0, 0, 0, 260, 260, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 193, 0, 0, 0, 276, 276, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 276,
+        274, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 276,
+        17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19
+      }
+    },
+    {
+      type = "objectgroup",
+      name = "Object Layer 1",
+      visible = true,
+      opacity = 1,
+      properties = {},
+      objects = {
+        {
+          name = "",
+          type = "player_spawn",
+          x = 1024,
+          y = 3456,
+          width = 256,
+          height = 256,
+          properties = {}
+        },
+        {
+          name = "",
+          type = "door",
+          x = 1152,
+          y = 1024,
+          width = 256,
+          height = 256,
+          properties = {}
+        },
+        {
+          name = "",
+          type = "shrinker",
+          x = 2176,
+          y = 2944,
+          width = 128,
+          height = 128,
+          properties = {}
+        },
+        {
+          name = "",
+          type = "shrinker",
+          x = 1152,
+          y = 2432,
+          width = 128,
+          height = 128,
+          properties = {}
+        }
+      }
+    }
+  }
+}
